531625e284e4ef9bec64d0353d72f4a58635d1fd
simoncarriere <simoncarriere@icloud.com>
2020-09-25 17:31:16 +0545

    Ofline Functionality)

--- sw.js
+++ sw.js
@@ -1,13 +1,17 @@
-const staticCacheName = "site-static";
+const staticCacheName = "site-static-v2";
+const dynamicCache = "site-dynamic-v1";
 const assets = [
   "/",
   "/index.html",
   "/js/app.js",
+  "/js/ui.js",
   "/js/materialize.min.js",
   "/css/styles.css",
   "/css/materialize.min.css",
   "/img/dish.png",
   "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
+  "/pages/fallback.html",
 ];
 
 // install SW
@@ -23,10 +27,40 @@ self.addEventListener("install", (evt) => {
 
 //Activate Event
 self.addEventListener("activate", (evt) => {
-  //  console.log("service worker has been activated");
+  // Delete old cach versions
+  evt.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCacheName && key !== dynamicCache)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
 });
 
 // fetch event
 self.addEventListener("fetch", (evt) => {
-  //  console.log("fetch event", evt);
+  evt.respondWith(
+    //async promise
+    caches
+      .match(evt.request)
+      .then((cacheRes) => {
+        // looks for existing cach | OR | fetch it
+        return (
+          cacheRes ||
+          fetch(evt.request).then((fetchRes) => {
+            // New Cache
+            return caches.open(dynamicCache).then((cache) => {
+              cache.put(evt.request.url, fetchRes.clone());
+              return fetchRes;
+            });
+          })
+        );
+      })
+      .catch(() => {
+        if (evt.request.url.indexOf(".html") > -1)
+          return caches.match("/pages/fallback.html");
+      })
+  );
 });
