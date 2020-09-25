ac36500ed8d724142d2b3d48ac9dec074ec40203
simoncarriere <simoncarriere@icloud.com>
2020-09-25 15:52:50 +0545

    Add assets to cash

--- sw.js
+++ sw.js
@@ -1,14 +1,32 @@
+const staticCacheName = "site-static";
+const assets = [
+  "/",
+  "/index.html",
+  "/js/app.js",
+  "/js/materialize.min.js",
+  "/css/styles.css",
+  "/css/materialize.min.css",
+  "/img/dish.png",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+];
+
 // install SW
 self.addEventListener("install", (evt) => {
-  console.log("service worker has been installed");
+  // SHELL CACHING
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log("caching shell assets");
+      cache.addAll(assets);
+    })
+  );
 });
 
 //Activate Event
 self.addEventListener("activate", (evt) => {
-  console.log("service worker has been activated");
+  //  console.log("service worker has been activated");
 });
 
 // fetch event
 self.addEventListener("fetch", (evt) => {
-  console.log("fetch event", evt);
+  //  console.log("fetch event", evt);
 });
